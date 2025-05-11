25dc3731fe000a2d77182dcd8458346841c8e68c
Dilip492 <dpal99113@gmail.com>
2025-05-11 13:45:49 +0530

    update the code

--- Routes/scrapRoute.js
+++ Routes/scrapRoute.js
@@ -3,27 +3,32 @@ const puppeteer = require("puppeteer");
 
 const router = express.Router();
 
-router.get('/getEvents/:county/:city' , async(req , res)=>{
+router.get('/getEvents/:county/:city', async (req, res) => {
 
-    
-    const Country = req.params.county ;
+
+    const Country = req.params.county;
     const city = req.params.city;
     try {
-        
-    const browser = await puppeteer.launch({ headless: true });
+
+        const browser = await puppeteer.launch({
+            headless: true,
+            executablePath: '/path/to/chrome', // or use process.env.CHROME_PATH
+            userDataDir: '/opt/render/.cache/puppeteer',
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
         const page = await browser.newPage();
-    
+
         const url = `https://www.eventbrite.com.au/d/${Country}--${city}/events/`;
-    
+
         await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 });
         await page.waitForSelector('section.discover-vertical-event-card');
-       
-        
+
+
         const eventList = await page.evaluate(() => {
             const cards = document.querySelectorAll('section.discover-vertical-event-card');
-            
+
             const events = [];
-    
+
             cards.forEach(card => {
                 const title = card.querySelector('h3')?.textContent.trim() || '';
                 const update = card.querySelector(".DiscoverVerticalEventCard-module__urgencySignals___7QVRD")?.textContent.trim() || '';
@@ -32,32 +37,32 @@ router.get('/getEvents/:county/:city' , async(req , res)=>{
                 const price = card.querySelector('.DiscoverVerticalEventCard-module__priceWrapper___usWo6 p')?.textContent.trim() || '';
                 const link = card.querySelector('a.event-card-link')?.href || '';
                 const image = card.querySelector('img.event-card-image')?.src || '';
-    
-                events.push({ title, date, location, price, link, image , update});
+
+                events.push({ title, date, location, price, link, image, update });
             });
-    
+
             return events;
         });
-    
-       
+
+
         await browser.close();
 
 
 
-        if(eventList == null){
-            return res.status(400).json({message:"Events list not found "})
+        if (eventList == null) {
+            return res.status(400).json({ message: "Events list not found " })
         }
 
 
-        res.status(200).json({data:eventList})
-       
-    
-   } catch (error) {
+        res.status(200).json({ data: eventList })
 
-    console.error(error.message);
-    res.status(500).json({message:"error fetching events", error:error.message})
-    
-   }
+
+    } catch (error) {
+
+        console.error(error.message);
+        res.status(500).json({ message: "error fetching events", error: error.message })
+
+    }
 
 
 
